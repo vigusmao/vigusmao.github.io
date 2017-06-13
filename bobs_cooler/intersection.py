3ba4ee3648bd464d92143ad5c308d1d075e91fa3
Vinicius Gusmao <vinicius@zwift.com>
2017-06-13 19:02:04 -0300

    adjusting MAX_VALUES so we have one percent overlap

--- bobs_cooler/intersection.py
+++ bobs_cooler/intersection.py
@@ -2,10 +2,6 @@ from random import randint
 from time import time
 
 
-
-MAX_VALUE = 10**5
-
-
 def intersection_n2(list1, list2):
     count = 0
     for element in list1:
@@ -23,10 +19,10 @@ def intersection_n(list1, list2):
             count += 1
     return count
 
-def create_list(size):
+def create_list(size, max_value):
     tempset = set()
     while len(tempset) < size:
-        tempset.add(randint(1, MAX_VALUE))
+        tempset.add(randint(1, max_value))
     return list(tempset)
        
 
@@ -39,9 +35,9 @@ def clock(method, arguments):
 
 
 while True:
-    N = eval(input(" Size: " ))
-    list1 = create_list(N)
-    list2 = create_list(N)
+    n = eval(input(" Size: " ))
+    list1 = create_list(n, 100*n)
+    list2 = create_list(n, 100*n)
     clock(intersection_n2, (list1, list2))
     clock(intersection_n, (list1, list2))
     print()
